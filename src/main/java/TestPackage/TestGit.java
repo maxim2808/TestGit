9936d1bab3ac2acfc752909bf3287f9ef9078d4c
max <maximt2808@gmail.com>
2023-09-03 12:01:27 +0300

    I commited file second time

--- src/main/java/TestPackage/TestGit.java
+++ src/main/java/TestPackage/TestGit.java
@@ -3,5 +3,6 @@ package TestPackage;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("changes have been made");
     }
 }
